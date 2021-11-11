ed8cbc6283940503c8e55ad366ca085cf14f893c
DanielPeretzz <93336006+DanielPeretzz@users.noreply.github.com>
2021-11-11 20:17:33 +0200

    Update Bonus.java

--- Bonus.java
+++ Bonus.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Bonus {
     static final Scanner SCANNER = new Scanner(System.in);
     public static void main(String[] args) {
-        Point Euclidean = new Point();
+        Point Euclideanp = new Point();
         System.out.println("enter 2 point");
         System.out.println("the first by separated : ");
         double y2 = SCANNER.nextDouble();
@@ -13,6 +13,6 @@ public class Bonus {
         System.out.println("the second by separated : ");
         double x2 = SCANNER.nextDouble();
         double x1 = SCANNER.nextDouble();
-        System.out.println(EuclideanPRO.Euclidean(y2,y1,x2,x1));
+        System.out.println(EuclideanP.Euclidean(y2,y1,x2,x1));
     }
 }
